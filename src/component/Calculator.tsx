7b4c759c7c66fdfd02ffe7283149ebcdf5684391
Daniel Kaiser <d.kaiser@wealthpilot.de>
2020-05-15 13:06:36 +0200

    Read values and add input fields.

--- src/component/Calculator.tsx
+++ src/component/Calculator.tsx
@@ -8,16 +8,48 @@
 //
 // - die Kurse, sowie die Währungen sollen vom Currencies-Server abgeholt werden
 
-import React from "react";
+import React, {ChangeEvent} from "react";
 import {fetchCurrencyForSymbol} from "../service/BackendService";
 import Select from "./Select";
 
+let firstIsoCode: string | undefined;
+let secondIsoCode: string | undefined;
 
-const Calculator = () =>
-    <div>
-        <div>1. Währung: <Select/></div>
-        <div>2. Währung: <Select/></div>
-    </div>
+function handleLabel(label: string, index: number) {
+    if (index === 1) {
+        firstIsoCode = label;
+    }
+    if (index === 2) {
+        secondIsoCode = label;
+    }
+}
+
+function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    if (firstIsoCode && secondIsoCode) {
+        if (e.target.name === 'firstValue') {
+            console.log('got first value');
+        }
+
+        if (e.target.name === 'secondValue') {
+            console.log('got second value');
+        }
+
+    }
+}
+
+const Calculator = () => {
+    return <div>
+        <div>1. Währung:
+            <Select onSelect={(label) => handleLabel(label, 1)}/>
+            <input name="firstValue" onChange={handleInputChange}/>
+        </div>
+
+        <div>2. Währung:
+            <Select onSelect={(label) => handleLabel(label, 2)}/>
+            <input name="secondValue" onChange={handleInputChange}/>
+        </div>
+    </div>;
+}
 
 fetchCurrencyForSymbol('ZAR').then(currency => {
     if (currency)
--- src/component/Select.tsx
+++ src/component/Select.tsx
@@ -1,22 +1,28 @@
 import React, {useEffect} from "react";
 import {fetchAllCurrencies} from "../service/BackendService";
 
+interface SelectProps {
+    onSelect: (label: string) => void
+}
 
-const Select = () => {
-    const [currencyValues, setCurrencyValues] = React.useState<Array<{ label: string, value: number }>>([]);
+const Select = (props: SelectProps) => {
+    const [loading, setLoading] = React.useState(true);
+    const [currencyValues, setCurrencyValues] = React.useState<Array<{ label: string }>>([{label: "Loading ..."}]);
 
     useEffect(() => {
         fetchAllCurrencies().then(currencies => {
-            if (currencies)
-                setCurrencyValues(currencies.map((currencyValue, index) => ({label: currencyValue.isoCode, value: index})))
+            if (currencies) {
+                setCurrencyValues(currencies.map((currencyValue) => ({label: currencyValue.isoCode})))
+                setLoading(false)
+            }
         })
-    }, [])
+    }, [currencyValues, props])
 
-    return <select>
+    return <select disabled={loading} onChange={e => props.onSelect(e.currentTarget.value)}>
         {currencyValues.map(item => (
             <option
-                key={item.value}
-                value={item.value}
+                key={item.label}
+                value={item.label}
             >
                 {item.label}
             </option>
