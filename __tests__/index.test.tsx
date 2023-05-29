42ef9b00dcbcf53d9304594915fd66d43b789340
Daniel Kaiser <danielkaiser@fastmail.com>
2023-05-30 01:32:19 +0200

    Correct broken test.

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import App from "../pages";
 
 test("renders heading", () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App isoCodes={["EUR", "USD"]} />);
   const headingElement = getByText(/Währungsrechner/i);
   expect(headingElement).toBeInTheDocument();
 });
